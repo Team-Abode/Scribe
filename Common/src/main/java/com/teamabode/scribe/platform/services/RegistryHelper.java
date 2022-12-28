f4dadca095c141ac75110d9a26cd0c2cc55affb5
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-12-28 13:49:33 -0500

    Changes

--- Common/src/main/java/com/teamabode/scribe/platform/services/RegistryHelper.java
+++ Common/src/main/java/com/teamabode/scribe/platform/services/RegistryHelper.java
@@ -1,9 +1,9 @@
-package com.teamabode.scribe;
+package com.teamabode.scribe.platform.services;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-public interface RegisteryInterface {
+public interface RegistryHelper {
     void setModId(String id);
     Item register(String identifier, Item item);
     Block register(String identifier, Block block);
--- Fabric/src/main/java/com/teamabode/scribe/Registery.java
+++ Fabric/src/main/java/com/teamabode/scribe/Registery.java
@@ -1,9 +1,10 @@
 package com.teamabode.scribe;
 
+import com.teamabode.scribe.platform.services.RegistryHelper;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-public class Registery implements RegisteryInterface {
+public class Registery implements RegistryHelper {
     @Override
     public void setModId(String id) {
         
