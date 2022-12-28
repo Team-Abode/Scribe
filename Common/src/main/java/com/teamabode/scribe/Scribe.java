8d7669b34fabbb95a6a1658cd12c9c1b092fa4ac
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-12-28 13:40:45 -0500

    Finish setup

--- Common/src/main/java/com/teamabode/scribe/Scribe.java
+++ Common/src/main/java/com/teamabode/scribe/Scribe.java
@@ -4,6 +4,7 @@ import com.teamabode.scribe.platform.Services;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
@@ -11,17 +12,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Scribe {
     public static final String MOD_ID = "scribe";
     public static final String MOD_NAME = "Scribe";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
+    public static final Supplier<Item> TEST_ITEM = () -> new Item(new Item.Properties());
+
     public static void init() {
         Scribe.LOGGER.info("Loading up Scribe!");
     }
 
-    // Example Hook
     public static void onItemTooltip(ItemStack stack, TooltipFlag context, List<Component> tooltip) {
         if (!stack.isEmpty()) {
             final FoodProperties food = stack.getItem().getFoodProperties();
--- Fabric/src/main/java/com/teamabode/scribe/ScribeFabric.java
+++ Fabric/src/main/java/com/teamabode/scribe/ScribeFabric.java
@@ -2,12 +2,16 @@ package com.teamabode.scribe;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
 
 public class ScribeFabric implements ModInitializer {
 
     public void onInitialize() {
         Scribe.init();
         Scribe.LOGGER.info("Hello Fabric World!");
+
+        Registry.register(Registry.ITEM, new ResourceLocation(Scribe.MOD_ID, "test_item"), Scribe.TEST_ITEM.get());
         
         // Fabric implementation of the example hook.
         ItemTooltipCallback.EVENT.register(Scribe::onItemTooltip);
