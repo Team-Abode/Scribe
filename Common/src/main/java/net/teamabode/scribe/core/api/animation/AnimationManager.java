c9cb041ad5eb602a42265ade4118df9360c67237
ExDrill <72329798+ExDrill@users.noreply.github.com>
2023-01-01 20:35:25 -0500

    Animation Resource Location changes

--- Common/src/main/java/net/teamabode/scribe/core/api/animation/AnimationManager.java
+++ Common/src/main/java/net/teamabode/scribe/core/api/animation/AnimationManager.java
@@ -1,26 +1,17 @@
 package net.teamabode.scribe.core.api.animation;
 
 import com.google.gson.JsonParser;
-import net.minecraft.client.animation.AnimationDefinition;
-import net.minecraft.client.animation.Keyframe;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
-import net.teamabode.scribe.core.Scribe;
 
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class AnimationManager extends SimplePreparableReloadListener<Map<ResourceLocation, AnimationData>> {
-
     public static final Map<ResourceLocation, AnimationData> ANIMATIONS = new HashMap<>();
-
-    public AnimationManager() {
-
-    }
 
     @Override
     protected Map<ResourceLocation, AnimationData> prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
@@ -28,7 +19,8 @@ public class AnimationManager extends SimplePreparableReloadListener<Map<Resourc
         resourceManager.getNamespaces().forEach(s -> {
             resourceManager.listResources("scribe_animations", resourceLocation -> resourceLocation.getPath().endsWith(".json")).forEach((resourceLocation, resource) -> {
                 try (Reader reader = resource.openAsReader()) {
-                    animations.put(resourceLocation, new AnimationData(JsonParser.parseReader(reader)));
+                    ResourceLocation trueLocation = new ResourceLocation(resourceLocation.getNamespace(), resourceLocation.getPath().substring(18, resourceLocation.getPath().length() - 5));
+                    animations.put(trueLocation, new AnimationData(JsonParser.parseReader(reader)));
                 } catch (Exception io) {
                     throw new AssertionError();
                 }
--- Common/src/main/java/net/teamabode/scribe/core/mixin/FrogModelMixin.java
+++ Common/src/main/java/net/teamabode/scribe/core/mixin/FrogModelMixin.java
@@ -1,14 +1,13 @@
 package net.teamabode.scribe.core.mixin;
 
-import net.minecraft.client.animation.AnimationDefinition;
 import net.minecraft.client.animation.definitions.FrogAnimation;
 import net.minecraft.client.model.FrogModel;
 import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.animal.frog.Frog;
-import net.teamabode.scribe.core.api.animation.AnimationParser;
 import net.teamabode.scribe.core.Scribe;
+import net.teamabode.scribe.core.api.animation.AnimationParser;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,21 +16,19 @@ import org.spongepowered.asm.mixin.Shadow;
 public class FrogModelMixin<T extends Frog> extends HierarchicalModel<T> {
     @Shadow @Final private ModelPart croakingBody;
 
+    private static final ResourceLocation FROG_CONSUME = new ResourceLocation(Scribe.MOD_ID, "consume");
+
     @Override
     public void setupAnim(T frog, float f, float g, float h, float i, float j) {
         this.root().getAllParts().forEach(ModelPart::resetPose);
         float k = Math.min((float)frog.getDeltaMovement().lengthSqr() * 200.0F, 8.0F);
         this.animate(frog.jumpAnimationState, FrogAnimation.FROG_JUMP, h);
         this.animate(frog.croakAnimationState, FrogAnimation.FROG_CROAK, h);
-        this.animate(frog.tongueAnimationState, this.getAnimation("consume"), h);
+        this.animate(frog.tongueAnimationState, AnimationParser.createAnimation(FROG_CONSUME), h);
         this.animate(frog.walkAnimationState, FrogAnimation.FROG_WALK, h, k);
         this.animate(frog.swimAnimationState, FrogAnimation.FROG_SWIM, h);
         this.animate(frog.swimIdleAnimationState, FrogAnimation.FROG_IDLE_WATER, h);
         this.croakingBody.visible = frog.croakAnimationState.isStarted();
-    }
-
-    public AnimationDefinition getAnimation(String name) {
-        return AnimationParser.createAnimation(new ResourceLocation(Scribe.MOD_ID, "scribe_animations/" + name + ".json"));
     }
 
     @Shadow
