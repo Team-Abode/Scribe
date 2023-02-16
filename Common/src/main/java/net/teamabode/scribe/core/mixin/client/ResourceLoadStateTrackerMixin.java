149cbb7abc14269cdf2ed0afd603a9ac9565020d
ExDrill <72329798+ExDrill@users.noreply.github.com>
2023-02-16 12:53:11 -0500

    Implement WeatheringBlock API and VerticalSlabBlock

--- Common/src/main/java/net/teamabode/scribe/core/mixin/client/ResourceLoadStateTrackerMixin.java
+++ Common/src/main/java/net/teamabode/scribe/core/mixin/client/ResourceLoadStateTrackerMixin.java
@@ -1,4 +1,4 @@
-package net.teamabode.scribe.core.mixin;
+package net.teamabode.scribe.core.mixin.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.ResourceLoadStateTracker;
